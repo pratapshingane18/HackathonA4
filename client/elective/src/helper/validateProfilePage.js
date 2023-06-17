3838c9b9a7c57df2adeffb48a2cf5d0c172d45e4
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-17 07:58:37 +0530

    Email verify function

---
+++ client/elective/src/helper/validateProfilePage.js
@@ -0,0 +1,21 @@
+import toast from 'react-hot-toast'
+import { authenticate } from './helper'
+
+/** validate profile page */
+export async function profileValidation(values){
+    const errors = emailVerify({}, values);
+    return errors;
+}
+
+/** validate email */
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong Email...!")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
+    }
+
+    return error;
+}
