ab70aee3af1335f4a3de0d4a14df2b55a410aa1f
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-17 08:01:40 +0530

    password function to reset password validate

--- client/elective/src/helper/validateresetpassword.js
+++ client/elective/src/helper/validateresetpassword.js
@@ -11,3 +11,22 @@ export async function resetPasswordValidation(values){
 
     return errors;
 }
+
+
+/** validate password */
+function passwordVerify(errors = {}, values){
+    /* eslint-disable no-useless-escape */
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
+}
