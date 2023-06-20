52cc0b54c0f7ada7e968e623f15e3d2b8586b625
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-20 22:01:21 +0530

    login part completed with database connectivity

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,32 +1,63 @@
-const express = require('express')
-const router = express.Router();
-const path = require('path')
+import { Router } from "express";
+const router = Router();
 
-//Controllers
-const register = require('../controller/register')
-
-
-
-
-//GET Request:
-
-router.get("/",(req,res)=>{
-    res.send("Hello");
-})
-
-//For posting judge and lawyer list as response
-router.get("/addCase",admin.PostData);
+/** import all controllers */
+import * as controller from '../controllers/appController.js';
+import { registerMail } from '../controllers/mailer.js'
+import Auth, { localVariables } from '../middleware/auth.js';
 
 
-//Post Request:
+
+/** POST Methods */
+router.route('/register').post(controller.register); // register user
+router.route('/registerMail').post(registerMail); // send the email
+router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
+router.route('/login').post(controller.verifyUser,controller.login); // login in app
+
+/** GET Methods */
+router.route('/user/:username').get(controller.getUser) // user with username
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP) // generate random OTP
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP) // verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession) // reset all the variables
 
 
-router.post("/register",register.register);
+/** PUT Methods */
+router.route('/updateuser').put(Auth, controller.updateUser); // is use to update the user profile
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword); // use to reset password
 
-//For Login: req.body -> user_id, password
-router.post("/login",register.login);
+
+
+export default router;
+
+// const express = require('express')
+// const router = express.Router();
+// const path = require('path')
+
+// //Controllers
+// const register = require('../controller/register')
+
+
+
+
+// //GET Request:
+
+// router.get("/",(req,res)=>{
+//     res.send("Hello");
+// })
+
+// //For posting judge and lawyer list as response
+// router.get("/addCase",admin.PostData);
+
+
+// //Post Request:
+
+
+// router.post("/register",register.register);
+
+// //For Login: req.body -> user_id, password
+// router.post("/login",register.login);
    
 
 
 
-module.exports = router;
+// module.exports = router;
