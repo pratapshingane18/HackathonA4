7d13b0d2c511f3adf2012c4ed99a92a0da02c279
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-20 21:31:50 +0530

    authorizing user route to protected one

--- client/elective/src/App.js
+++ client/elective/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
-/* Importing all components*/
-
+/** import all components */
 import Username from './components/Username';
 import Password from './components/Password';
 import Register from './components/Register';
@@ -12,46 +11,46 @@ import Recovery from './components/Recovery';
 import Reset from './components/Reset';
 import PageNotFound from './components/PageNotFound';
 
-/* root router */
+
+/** auth middleware */
+import { AuthorizeUser, ProtectRoute } from './middleware/auth'
+
+/** root routes */
 const router = createBrowserRouter([
-  {
-    path : '/',
-    element : <Username></Username>
-},
-{
-    path : '/register',
-    element : <Register></Register>
-},
-{
-    path : '/password',
-    element : <ProtectRoute><Password /></ProtectRoute>
-},
-{
-    path : '/profile',
-    element : <AuthorizeUser><Profile /></AuthorizeUser>
-},
-{
-    path : '/recovery',
-    element : <Recovery></Recovery>
-},
-{
-    path : '/reset',
-    element : <Reset></Reset>
-},
-{
-    path : '*',
-    element : <PageNotFound></PageNotFound>
-},
-
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <ProtectRoute><Password /></ProtectRoute>
+    },
+    {
+        path : '/profile',
+        element : <AuthorizeUser><Profile /></AuthorizeUser>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '*',
+        element : <PageNotFound></PageNotFound>
+    },
 ])
-const App = () => {
-  return (
-   <main>
-    <RouterProvider router = {router}>
 
-    </RouterProvider>
-   </main>
+export default function App() {
+  return (
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
-
-export default App
