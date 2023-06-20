8ed2fc3060e6391800b2830ecb6670da4f37b9a0
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-20 19:45:42 +0530

    created mail generator object

--- server/config.js
+++ server/config.js
@@ -1,3 +1,6 @@
 export default {
-    JWT_SECRET : "SmtJrMltSsNDlbrt/QfUijEzqWE4HDKif3IvAVWVSr4="
+    JWT_SECRET : "SmtJrMltSsNDlbrt/QfUijEzqWE4HDKif3IvAVWVSr4=",
+    EMAIL: "angeline.haag@ethereal.email",
+    PASSWORD: "w7ZhKmWecpt8FVB8mM"
 }
+
