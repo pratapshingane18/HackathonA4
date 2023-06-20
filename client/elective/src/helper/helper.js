588a6443d95293b38324c1ab25a0e87faacd418b
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-20 20:06:18 +0530

    adding register endpoint

--- client/elective/src/helper/helper.js
+++ client/elective/src/helper/helper.js
@@ -19,3 +19,22 @@ export async function authenticate(username){
         return { error : "Username doesn't exist...!"}
     }
 }
+
+
+/** register user function */
+export async function registerUser(credentials){
+    try {
+        const { data : { msg }, status } = await axios.post(`/api/register`, credentials);
+
+        let { username, email } = credentials;
+
+        /** send email */
+        if(status === 201){
+            await axios.post('/api/registerMail', { username, userEmail : email, text : msg})
+        }
+
+        return Promise.resolve(msg)
+    } catch (error) {
+        return Promise.reject({ error })
+    }
+}
