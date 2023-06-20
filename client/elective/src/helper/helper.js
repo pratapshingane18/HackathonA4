c4fc848530c950a23db78f2ec5c47b64a9914e4e
pratapshingane18 <sushrutrao465@gmail.com>
2023-06-20 20:04:35 +0530

    adding getusername endpoint

--- client/elective/src/helper/helper.js
+++ client/elective/src/helper/helper.js
@@ -1,6 +1,16 @@
 import axios from axios;
 
 
+/** To get username from Token */
+export async function getUsername(){
+    const token = localStorage.getItem('token')
+    if(!token) return Promise.reject("Cannot find Token");
+    let decode = jwt_decode(token)
+    return decode;
+}
+
+
+
 /** authenticate function */
 export async function authenticate(username){
     try {
