e66596d5e4d62a53fcd9ac1e487856f444792d20
aman <amanmagrawal2002@gmail.com>
2023-06-22 19:15:30 +0530

    seperating the functions

---
+++ server/controllers/otpGenerator.js
@@ -0,0 +1,24 @@
+import UserModel from '../model/User.model.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js'
+import otpGenerator from 'otp-generator';
+
+
+/** GET: http://localhost:8080/api/generateOTP */
+export async function generateOTP(req,res){
+    req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false})
+    res.status(201).send({ code: req.app.locals.OTP })
+}
+
+
+/** GET: http://localhost:8080/api/verifyOTP */
+export async function verifyOTP(req,res){
+    const { code } = req.query;
+    if(parseInt(req.app.locals.OTP) === parseInt(code)){
+        req.app.locals.OTP = null; // reset the OTP value
+        req.app.locals.resetSession = true; // start session for reset password
+        return res.status(201).send({ msg: 'Verify Successsfully!'})
+    }
+    return res.status(400).send({ error: "Invalid OTP"});
+}
--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,131 +1,254 @@
-const bcrypt = require("bcrypt");
-const Student = require("../models/student");
+import UserModel from '../model/User.model.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js';
+import otpGenerator from 'otp-generator';
 
-class Register {
-  static register = async (req, res) => {
-    
+/** POST: http://localhost:8080/api/register 
+ * @param : {
+  "username" : "example123",
+  "password" : "admin123",
+  "email": "example@gmail.com",
+  "firstName" : "bill",
+  "lastName": "william",
+  "mobile": 8009860560,
+  "address" : "Apt. 556, Kulas Light, Gwenborough",
+  "profile": ""
+}
+*/
+export async function register(req,res){
 
-    //Body properties
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const userId = req.body.userId;
-    const password = req.body.password;
-    const cpassword = req.body.cpassword;
-    const email = req.body.email;
-    const branch = req.body.branch;
-    const year = req.body.year;
-    const degree = req.body.degree;
-    
+  try {
+      const { username, password, profile, email } = req.body;        
 
-    //Checking if exist or not
-    const user = await Student.findOne({ userId: userId }).lean();
+      // check the existing user
+      const existUsername = new Promise((resolve, reject) => {
+          UserModel.findOne({ username }, function(err, user){
+              if(err) reject(new Error(err))
+              if(user) reject({ error : "Please use unique username"});
 
-    if (user) {
-      res.json({ status: "failed", message: "User Already Exist!!" });
-      console.log(user);
-    } else {
-      //Checking all properties are fulfilled
-      if (firstname && userId && password) {
-        //matching the passwords
-        if (password == cpassword) {
-          try {
-            //Generating a hash
-            const salt = await bcrypt.genSalt(10);
-            const hash_password = await bcrypt.hash(password, salt);
-
-            //Building a model document of user
-            const doc = new Student({
-              firstname: firstname,
-              lastname: lastname,
-              userId: userId,
-              password: password,
-              email: email,
-              branch: branch,
-              year: year,
-              degree: degree
-            });
-
-            //saving it
-            await doc.save();
-
-            const saved = await Student.findOne({ user_id: userId });
-
-            if (saved) {
-              res.json({
-                status: "success",
-                message: "Registration Successful",
-              });
-            } else {
-              res.json({
-                status: "failed",
-                message: "Registration Unsuccessful and not saved",
-              });
-            }
-          } catch (err) {
-            console.log(err);
-            res.json({
-              status: "failed",
-              message: "Registration UnSuccessful",
-            });
-          }
-        } else {
-          res.json({
-            status: "failed",
-            message: "Password and Confirm Password wasn't matching",
-          });
-        }
-      } else {
-        res.json({ status: "failed", message: "All feilds are required" });
-      }
-    }
-  };
-
-
-
-
-  static login = async (req, res) => {
-    
-
-    const userId = req.body.userId;
-    const password = req.body.password;
-    
-
-    const user = await Student.findOne({ user_id: userId }).lean();
-
-    if (user) {
-      if (password) {
-        const checkpass = await bcrypt.compareSync(password, user.password);
-
-        if (user.user_id === userId && checkpass) {
-          console.log("Login Successfully");
-          res.status(201).json({
-            status: "success",
-            message: "Login Successful",
-
-            userId: user.userId,
-            
-          });
-        } else {
-          console.log("Login Unsuccessful");
-          const result = {
-            status: "failed",
-            message: "Please check the credentials",
-          };
-
-          res.json(result);
-        }
-      } else {
-        console.log("All feilds are required");
-        res.json({ status: "failed", message: "All feilds are required" });
-      }
-    } else {
-      console.log("User not found! Please registe");
-      res.json({
-        status: "failed",
-        message: "User not found! Please register",
+              resolve();
+          })
       });
-    }
-  };
+
+      // check for existing email
+      const existEmail = new Promise((resolve, reject) => {
+          UserModel.findOne({ email }, function(err, email){
+              if(err) reject(new Error(err))
+              if(email) reject({ error : "Please use unique Email"});
+
+              resolve();
+          })
+      });
+
+
+      Promise.all([existUsername, existEmail])
+          .then(() => {
+              if(password){
+                  bcrypt.hash(password, 10)
+                      .then( hashedPassword => {
+                          
+                          const user = new UserModel({
+                              username,
+                              password: hashedPassword,
+                              profile: profile || '',
+                              email
+                          });
+
+                          // return save result as a response
+                          user.save()
+                              .then(result => res.status(201).send({ msg: "User Register Successfully"}))
+                              .catch(error => res.status(500).send({error}))
+
+                      }).catch(error => {
+                          return res.status(500).send({
+                              error : "Enable to hashed password"
+                          })
+                      })
+              }
+          }).catch(error => {
+              return res.status(500).send({ error })
+          })
+
+
+  } catch (error) {
+      return res.status(500).send(error);
+  }
+
 }
 
-module.exports = Register;
+
+export async function login(req,res){
+   
+  const { username, password } = req.body;
+
+  try {
+      
+      UserModel.findOne({ username })
+          .then(user => {
+              bcrypt.compare(password, user.password)
+                  .then(passwordCheck => {
+
+                      if(!passwordCheck) return res.status(400).send({ error: "Don't have Password"});
+
+                      // create jwt token
+                      const token = jwt.sign({
+                                      userId: user._id,
+                                      username : user.username
+                                  }, ENV.JWT_SECRET , { expiresIn : "24h"});
+
+                      return res.status(200).send({
+                          msg: "Login Successful...!",
+                          username: user.username,
+                          token
+                      });                                    
+
+                  })
+                  .catch(error =>{
+                      return res.status(400).send({ error: "Password does not Match"})
+                  })
+          })
+          .catch( error => {
+              return res.status(404).send({ error : "Username not Found"});
+          })
+
+  } catch (error) {
+      return res.status(500).send({ error});
+  }
+}
+
+
+
+
+
+// const bcrypt = require("bcrypt");
+// const Student = require("../models/student");
+
+// class Register {
+//   static register = async (req, res) => {
+    
+
+//     //Body properties
+//     const firstname = req.body.firstname;
+//     const lastname = req.body.lastname;
+//     const userId = req.body.userId;
+//     const password = req.body.password;
+//     const cpassword = req.body.cpassword;
+//     const email = req.body.email;
+//     const branch = req.body.branch;
+//     const year = req.body.year;
+//     const degree = req.body.degree;
+    
+
+//     //Checking if exist or not
+//     const user = await Student.findOne({ userId: userId }).lean();
+
+//     if (user) {
+//       res.json({ status: "failed", message: "User Already Exist!!" });
+//       console.log(user);
+//     } else {
+//       //Checking all properties are fulfilled
+//       if (firstname && userId && password) {
+//         //matching the passwords
+//         if (password == cpassword) {
+//           try {
+//             //Generating a hash
+//             const salt = await bcrypt.genSalt(10);
+//             const hash_password = await bcrypt.hash(password, salt);
+
+//             //Building a model document of user
+//             const doc = new Student({
+//               firstname: firstname,
+//               lastname: lastname,
+//               userId: userId,
+//               password: password,
+//               email: email,
+//               branch: branch,
+//               year: year,
+//               degree: degree
+//             });
+
+//             //saving it
+//             await doc.save();
+
+//             const saved = await Student.findOne({ user_id: userId });
+
+//             if (saved) {
+//               res.json({
+//                 status: "success",
+//                 message: "Registration Successful",
+//               });
+//             } else {
+//               res.json({
+//                 status: "failed",
+//                 message: "Registration Unsuccessful and not saved",
+//               });
+//             }
+//           } catch (err) {
+//             console.log(err);
+//             res.json({
+//               status: "failed",
+//               message: "Registration UnSuccessful",
+//             });
+//           }
+//         } else {
+//           res.json({
+//             status: "failed",
+//             message: "Password and Confirm Password wasn't matching",
+//           });
+//         }
+//       } else {
+//         res.json({ status: "failed", message: "All feilds are required" });
+//       }
+//     }
+//   };
+
+
+
+
+//   static login = async (req, res) => {
+    
+
+//     const userId = req.body.userId;
+//     const password = req.body.password;
+    
+
+//     const user = await Student.findOne({ user_id: userId }).lean();
+
+//     if (user) {
+//       if (password) {
+//         const checkpass = await bcrypt.compareSync(password, user.password);
+
+//         if (user.user_id === userId && checkpass) {
+//           console.log("Login Successfully");
+//           res.status(201).json({
+//             status: "success",
+//             message: "Login Successful",
+
+//             userId: user.userId,
+            
+//           });
+//         } else {
+//           console.log("Login Unsuccessful");
+//           const result = {
+//             status: "failed",
+//             message: "Please check the credentials",
+//           };
+
+//           res.json(result);
+//         }
+//       } else {
+//         console.log("All feilds are required");
+//         res.json({ status: "failed", message: "All feilds are required" });
+//       }
+//     } else {
+//       console.log("User not found! Please registe");
+//       res.json({
+//         status: "failed",
+//         message: "User not found! Please register",
+//       });
+//     }
+//   };
+// }
+
+// module.exports = Register;
