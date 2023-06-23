5c62164a34ee7413317815adc789d8b6b2bb542d
aman <amanmagrawal2002@gmail.com>
2023-06-23 21:51:52 +0530

    change in routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,14 +4,18 @@ const router = Router();
 /** import all controllers */
 import * as controller from '../controllers/appController.js';
 import { registerMail } from '../controllers/mailer.js'
+
+/* import middlewares */
 import Auth, { localVariables } from '../middleware/auth.js';
+import verifyUser from "../middleware/verify";
 
 
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
 router.route('/registerMail').post(registerMail); // send the email
-router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
+// router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
+router.route('/authenticate').post(verifyUser, (req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser,controller.login); // login in app
 
 /** GET Methods */
